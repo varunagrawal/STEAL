91c7d5b05ff6a35ecfd2a07d0dcfcd26b1150a46
Varun Agrawal <varagrawal@gmail.com>
2022-12-06 03:54:58 +0530

    make imports for the learning module

--- steal/learning/__init__.py
+++ steal/learning/__init__.py
@@ -1,3 +1,15 @@
 """Module for help with learning of trajectory data."""
 
+from steal.learning.base import *
+from steal.learning.context_nets import *
 from steal.learning.controllers import *
+from steal.learning.loss import *
+from steal.learning.taskmap_nets import *
+from steal.learning.trainer import *
+
+
+class Params(object):
+    """Helper class to record various parameters."""
+    def __init__(self, **kwargs):
+        super(Params, self).__init__()
+        self.__dict__.update(kwargs)
