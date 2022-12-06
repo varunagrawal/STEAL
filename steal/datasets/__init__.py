c37eee3b55536d7772bf9d657d92a5912ff59760
Varun Agrawal <varagrawal@gmail.com>
2022-12-06 17:07:03 +0530

    add lasa module for handling the LASA dataset

--- steal/datasets/__init__.py
+++ steal/datasets/__init__.py
@@ -1,6 +1,5 @@
 """Module for dealing with datasets."""
 
-import pyLasaDataset as lasa
-
 from steal.datasets.datasets import *
+from steal.datasets.lasa import *
 from steal.datasets.preprocess import *
