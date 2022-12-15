1a8860836b9328b47aba413d643789c46be7c220
Varun Agrawal <varagrawal@gmail.com>
2022-12-15 18:43:25 +0530

    format and save samples

--- scripts/shivika02_gp_sampling.py
+++ scripts/shivika02_gp_sampling.py
@@ -4,6 +4,8 @@ Script to train a GP and sample from it.
 python scripts/shivika02_gp_sampling.py
 """
 
+import argparse
+
 import gpytorch
 import torch
 
@@ -12,9 +14,26 @@ from steal.gp.multi_task import MultitaskApproximateGaussianProcess
 from steal.utils.plotting.gp import plot_samples
 
 
+def parse_args():
+    """Parse command line arguments"""
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--sample_ppd',
+        action='store_true',
+        help=
+        "Sample from the posterior predictive distribution instead of the GP posterior"
+    )
+    parser.add_argument('--dataset',
+                        default="Sshape",
+                        help="The shape name in the LASA dataset.")
+    return parser.parse_args()
+
+
 def main():
     """Main runner"""
-    trajectories = load_trajectories()
+    args = parse_args()
+
+    trajectories = load_trajectories(dataset_name=args.dataset)
 
     # Concatenating the demos
     train_t, train_xy = concatenate_trajectories(trajectories)
@@ -32,11 +51,18 @@ def main():
     training_iterations = 60
     gp.train(train_t, train_xy, training_iterations=training_iterations)
 
-    test_t = torch.linspace(0, 6, 1000).double()
+    test_t = torch.linspace(0, 4.8, 1000).double()
 
     # Compute posterior for the test data
-    posterior = gp.posterior(test_t)
+    if args.sample_ppd:
+        print("Sampling from PPD")
+        posterior = gp.evaluate(test_t)
+    else:
+        posterior = gp.posterior(test_t)
+
     samples = gp.samples(test_t, 10)
+
+    torch.save(samples, open("{args.dataset.lower()}_gp_samples.pt", 'wb'))
 
     # Compute the confidence interval for the posterior
     with torch.no_grad(), gpytorch.settings.fast_pred_var():
@@ -44,14 +70,22 @@ def main():
         lower, upper = posterior.confidence_region()
 
     x_lim = [0, 6.0]
-    y_lims = [[-40, 15], [-25, 30]]
+    y_lims = [[-15, 45], [-10, 60]]
     legend_list = [
         'Sample 1', 'Sample 2', 'Sample 3', 'Sample 4', 'Sample 5', 'Sample 6',
         'Sample 7', 'Sample 8', 'Sample 9', 'Sample 10', 'Mean', 'Confidence'
     ]
     # Sample visualization
-    plot_samples(num_tasks, test_t, samples, mean, lower, upper, legend_list,
-                 x_lim, y_lims)
+    plot_samples(num_tasks,
+                 test_t,
+                 samples,
+                 mean,
+                 lower,
+                 upper,
+                 legend_list,
+                 x_lim,
+                 y_lims,
+                 title=f"Sampled Trajectories {args.sample_ppd}")
 
 
 if __name__ == "__main__":
